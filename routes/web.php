a2f9ff318a1be21e9d267d697a3c29efe07906dc
Raquel <raquel.evangelista2@aluno.ce.gov.br>
2025-11-02 18:18:19 -0300

    feat: adiciona rota para resource de pratos

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\PratoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('categorias', CategoriaController::class);
+
+Route::resource('pratos', PratoController::class);
