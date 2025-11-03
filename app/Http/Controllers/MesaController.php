0a294e6a43f84d706044a09aa42f6f359e4ef79f
Raquel <raquel.evangelista2@aluno.ce.gov.br>
2025-11-02 22:37:15 -0300

    feat: cria MesaController com métodos para CRUD

---
+++ app/Http/Controllers/MesaController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mesa;
+use Illuminate\Http\Request;
+
+class MesaController extends Controller
+{
+    public function index()
+    {
+        $mesas = Mesa::all();
+        return view('mesas.index', compact('mesas'));
+    }
+
+    public function create()
+    {
+        return view('mesas.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'numero' => 'required|numeric|unique:mesas,numero',
+            'capacidade' => 'required|numeric',
+            'status' => 'required|string|max:255',
+        ], [
+            'numero.unique' => 'Já existe uma mesa com esse número.',
+        ]);
+
+        Mesa::create($validated);
+
+        return redirect()->route('mesas.index')->with('success', 'Mesa ' .$request->numero. ' cadastrada com sucesso!');
+    }
+
+    public function edit(Mesa $mesa)
+    {
+        return view('mesas.edit', compact('mesa'));
+    }
+
+    public function update(Request $request, Mesa $mesa)
+    {
+        $validated = $request->validate([
+            'numero' => 'required|numeric|unique:mesas,numero,' . $mesa->id,
+            'capacidade' => 'required|numeric',
+            'status' => 'required|string|max:255',
+        ], [
+            'numero.unique' => 'Já existe uma mesa com esse número.',
+        ]);
+
+        $mesa->update($validated);
+
+        return redirect()->route('mesas.index')->with('success', 'Mesa ' .$mesa->numero. ' atualizada com sucesso!');
+    }
+
+    public function destroy(Mesa $mesa)
+    {
+        $mesa->delete();
+
+        return redirect()->route('mesas.index')->with('success', 'Mesa ' .$mesa->numero.' deletada com sucesso!');
+    }
+}
