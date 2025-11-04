44d0732aaa4c0f12af00f76fbee332b153915cd8
Raquel <raquel.evangelista2@aluno.ce.gov.br>
2025-11-03 22:33:00 -0300

    feat: cria view para cadastrar novo funcionário

---
+++ resources/views/funcionarios/create.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Novo funcionário</title>
+</head>
+<body>
+    <h1>Cadastrar funcionário</h1>
+
+    <form action="{{ route('funcionarios.store') }}" method="POST">
+        @csrf
+        <p>
+            <label>Nome:</label><br>
+            <input type="text" name="nome" value="{{ old('nome') }}" required>
+        </p>
+        <p>
+            <label>Email:</label><br>
+            <input type="email" name="email" value="{{ old('email') }}" required>
+        </p>
+        <p>
+            <label>Telefone:</label><br>
+            <input type="text" name="telefone" value="{{ old('telefone') }}" required>
+        </p>
+        <p>
+            <label>Cargo:</label><br>
+            <input type="text" name="cargo" value="{{ old('cargo') }}" required>
+        </p>
+
+        <button type="submit">Salvar</button>
+    </form>
+
+    <a href="{{ route('funcionarios.index') }}">Voltar</a>
+</body>
+</html>
