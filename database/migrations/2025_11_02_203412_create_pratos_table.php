f9f6e0b1a61f7d450e6465fefa1cb81df1867092
Raquel <raquel.evangelista2@aluno.ce.gov.br>
2025-11-08 22:09:24 -0300

    feat: adiciona coluna para chave estrangeira de categoria

--- database/migrations/2025_11_02_203412_create_pratos_table.php
+++ database/migrations/2025_11_02_203412_create_pratos_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->text('descricao')->nullable();
             $table->decimal('preco', 8, 2);
             $table->string('imagem')->nullable();
+            $table->foreignId('categoria_id')->constrained('categorias')->onDelete('cascade');
             $table->timestamps();
         });
     }
