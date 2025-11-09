b6ce45f0c78017369d979c520720c0f4aa2714c0
Raquel <raquel.evangelista2@aluno.ce.gov.br>
2025-11-08 22:14:20 -0300

    feat: adiciona relacionamento 1-> N  entre categoria e prato

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Categoria extends Model
 {
     protected $fillable = ['nome', 'descricao'];
+
+    public function pratos()
+    {
+        return $this->hasMany(Prato::class);
+    }
 }
