18fef54ebb37e1c70ae092d69e5a988f22328856
Raquel <raquel.evangelista2@aluno.ce.gov.br>
2025-11-13 21:14:30 -0300

    feat: adiciona rota para crud de pedidos

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\PratoController;
 use App\Http\Controllers\MesaController;
 use App\Http\Controllers\FuncionarioController;
+use App\Http\Controllers\PedidoController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,3 +19,5 @@ Route::resource('pratos', PratoController::class);
 Route::resource('mesas', MesaController::class);
 
 Route::resource('funcionarios', FuncionarioController::class);
+
+Route::resource('pedidos', PedidoController::class);
