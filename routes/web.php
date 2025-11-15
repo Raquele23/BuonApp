33eeb619ba1c84a9541bebd0d299a9736c931fe9
Raquel <raquel.evangelista2@aluno.ce.gov.br>
2025-11-15 09:51:16 -0300

    feat: implementa laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\PratoController;
@@ -12,12 +13,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('categorias', CategoriaController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('pratos', PratoController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-Route::resource('mesas', MesaController::class);
+    Route::resource('categorias', CategoriaController::class);
+    Route::resource('pratos', PratoController::class);
+    Route::resource('mesas', MesaController::class);
+    Route::resource('funcionarios', FuncionarioController::class);
+    Route::resource('pedidos', PedidoController::class);
+});
 
-Route::resource('funcionarios', FuncionarioController::class);
-
-Route::resource('pedidos', PedidoController::class);
+require __DIR__.'/auth.php';
