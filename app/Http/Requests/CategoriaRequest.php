3c0250b471a86ab3e254b6221813781cef3edb7a
Raquel <raquel.evangelista2@aluno.ce.gov.br>
2025-11-28 21:39:53 -0300

    feat: adiciona FormRequest de Categoria

---
+++ app/Http/Requests/CategoriaRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoriaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required|string|max:30',
+            'descricao' => 'nullable|string|max:150',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'descricao.string' => 'A descrição deve ser um texto válido.',
+            'descricao.max' => 'A descrição não pode ter mais de 150 caracteres.',
+        ];
+    }
+}
